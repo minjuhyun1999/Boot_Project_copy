c13e740f566cdb8b12422af42e6c327a6f806d14
minjuhyun1999 <minjoohyun67@naver.com>
2025-07-18 09:33:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Boot_Project_copy" 
---
+++ src/main/java/com/busanit501/boot_project/BootProjectCopyApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjectCopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootProjectCopyApplication.class, args);
+    }
+
+}
